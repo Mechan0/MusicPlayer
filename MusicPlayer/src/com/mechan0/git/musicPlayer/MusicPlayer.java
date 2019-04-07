ff756d5966ff1e17e4ae6710c19967c20b792e40
Mechan0 <bubb4h0t3p@gmail.com>
2019-04-07 14:08:59 -0400

    Added JavaFX SDK Library

--- MusicPlayer/src/com/mechan0/git/musicPlayer/MusicPlayer.java
+++ MusicPlayer/src/com/mechan0/git/musicPlayer/MusicPlayer.java
@@ -1,7 +1,16 @@
 package com.mechan0.git.musicPlayer;
 
-public class MusicPlayer {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MusicPlayer extends Application {
 	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
 		
 	}
 }
