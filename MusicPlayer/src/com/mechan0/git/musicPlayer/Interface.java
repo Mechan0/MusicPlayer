2091fd634772b4f459a78430bf3901ae667d7c56
Mechan0 <bubb4h0t3p@gmail.com>
2019-04-07 17:07:24 -0400

    Created basic interfaces
    - Added basic interfaces for account selection/creation
    - Added TODO: sections for functional components

---
+++ MusicPlayer/src/com/mechan0/git/musicPlayer/Interface.java
@@ -0,0 +1,85 @@
+package com.mechan0.git.musicPlayer;
+
+import java.util.Comparator;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Interface {
+	Stage window;
+
+	Scene userAccountScene, newUserAccountScene;
+
+	public Interface() {
+		
+	}
+	public void initialiseInterface (Stage primaryStage) {
+		window = primaryStage;
+		window.setTitle("Music Player");
+		// 1: user account scene
+		newUserAccountScene = createNewUserAccountScene();
+		userAccountScene = createUserAccountScene();
+        primaryStage.setScene(userAccountScene);
+        primaryStage.show();
+	}
+	public Scene createUserAccountScene() {
+		ListView<String> userAccounts = new ListView<String>();
+		// TODO: load users from database file
+		ObservableList<String> users = FXCollections.observableArrayList (
+			    "James", "Alex", "Tony", "Brandon");
+		users.sort(Comparator.naturalOrder());
+		Button newUser = new Button("Create New User");
+		// lambda on button press load newAccountScene window
+		newUser.setOnAction(e -> window.setScene(newUserAccountScene));
+		Button loadUser = new Button("Login as user");
+		loadUser.setOnAction(e -> {
+			//TODO: Retrieve text box value account and load corresponding panel
+		});
+		userAccounts.setItems(users);
+		VBox userAccountLayout = new VBox(20);
+		userAccountLayout.getChildren().addAll(new Label("Users: "), userAccounts, loadUser, newUser);
+		return new Scene(userAccountLayout, 200, 200);
+	}
+	public Scene createNewUserAccountScene() {
+		//Creating a GridPane container
+		GridPane newAccLayout = new GridPane();
+		
+		newAccLayout.setPadding(new Insets(10, 10, 10, 10));
+		newAccLayout.setVgap(5);
+		newAccLayout.setHgap(5);
+		// Defining header label
+		Label headLabel = new Label("Username:");
+		//GridPane.setConstraints(headLabel, 1, 0);
+		newAccLayout.getChildren().add(new Label("Username"));
+		//Defining the userName text field
+		final TextField userName = new TextField();
+		userName.setPromptText("Enter your userName.");
+		userName.setPrefColumnCount(10);
+		userName.getText();
+		GridPane.setConstraints(userName, 1, 0);
+		newAccLayout.getChildren().add(userName);
+		//Defining the Submit button
+		Button submit = new Button("Submit");
+		submit.setOnAction(e -> {
+			//TODO: add value of textbox to user accounts
+			window.setScene(userAccountScene);
+		});
+		GridPane.setConstraints(submit, 2, 0);
+		newAccLayout.getChildren().add(submit);
+		// Defining return button
+		Button returnButton = new Button("Cancel");
+		returnButton.setOnAction(e -> window.setScene(userAccountScene));
+		GridPane.setConstraints(returnButton, 0, 1);
+		newAccLayout.getChildren().add(returnButton);
+		
+		return new Scene(newAccLayout, 200, 200);
+	}
+}
--- MusicPlayer/src/com/mechan0/git/musicPlayer/MusicPlayer.java
+++ MusicPlayer/src/com/mechan0/git/musicPlayer/MusicPlayer.java
@@ -1,16 +1,16 @@
 package com.mechan0.git.musicPlayer;
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class MusicPlayer extends Application {
+	Stage window;
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void start(Stage primaryStage) throws Exception {
+		Interface inter = new Interface();
+		inter.initialiseInterface(primaryStage);
 	}
 }
